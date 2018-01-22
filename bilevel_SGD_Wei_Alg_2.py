2639a310dd740db9f87b6b0c7799f84d3797b454
Wei <wjiang1990@gmail.com>
2018-01-22 14:53:26 -0500

    test algorithm 2, works fine, converge very fast, but very sensitie to the step size of C

--- bilevel_SGD_Wei_Alg_2.py
+++ bilevel_SGD_Wei_Alg_2.py
@@ -7,22 +7,85 @@ import numpy as np
 import random as rd
 import pandas as pd
 from dynamic_plot import dynamic_plot
+import matplotlib.pyplot as plt
 
 
 
-class bilevel_SGD():
+class bilevel_SGD_Alg2():
 
     def __init__(self):
         self.C = None
         self.alpha = None
-        self.beta = np.random.uniform(-1,1)
+        self.beta = None
         self.C_min = 1e-4
         self.C_max = 1e6
-        self.t_max = 2000 # maximal number of iterations
+        self.t_max = 300 # maximal number of iterations
         self.lr_beta = 0.001 # learning rate (step size) for beta
-        self.lr_C = 1 # learning rate for C
+        self.lr_C = 0.001 # learning rate for C
 
         self.accuracy_threshold = 0.97
+
+        self.duality_gap_tol = 1e-3
+        self.C_ls = []
+        self.loss_ls = []
+        self.accuracy_ls =[]
+
+    def dual_CD_step(self):
+        """
+        Do dual coordinate descent on validation set.
+        params:
+            X: array, each row is a sample
+            y: 1d array, prediction label
+        """
+
+        # temp_Q = np.dot(y, X)
+        # self.Q = np.dot(temp_Q, np.transpose(temp_Q))
+
+        if not self._check_optimality(method='duality_gap'): # if alpha is not optimal
+            ind_permuted = np.random.permutation(self.X_t_size)
+            for ind in ind_permuted:
+                temp_alpha = self.alpha[ind]
+                self.Gradient[ind] = self.y_train[ind]* np.dot(self.beta, self.X_train[ind,]) - 1
+
+                # print self.Gradient[ind]
+
+                if temp_alpha == 0:
+                    self.Proj_grad[ind] = min(self.Gradient[ind], 0)
+                elif temp_alpha == self.C:
+                    self.Proj_grad[ind] = max(self.Gradient[ind], 0)
+                elif 0 < temp_alpha < self.C:
+                    self.Proj_grad[ind] = self.Gradient[ind]
+
+                if not np.isclose(self.Proj_grad[ind], 1e-9):
+                    self.alpha[ind] = min(max(temp_alpha - self.Gradient[ind]/ self.Q_ii[ind], 0), self.C)
+                    self.beta = self.beta + self.y_train[ind]*(self.alpha[ind] - temp_alpha)*self.X_train[ind,]
+
+    def _check_optimality(self, method):
+        """
+        Check optimality condition using duality gap
+        params:
+            method: str,'duality_gap'; 
+                    'gradient': check if all projected gradient are close to 0
+                    'gradient_gap': check if the difference between largest and smallest projected gradients are less than a tolerance
+        """
+        if method == 'duality_gap':
+            self._duality_gap()
+            # print self.gap
+            
+            if self.gap <= self.duality_gap_tol:
+
+                return True
+            else:
+                return False
+        elif method == 'gradient':
+            return all(np.isclose(self.Proj_grad,0))
+        elif method == 'gradient_gap':
+            gap = max(self.Proj_grad) - min(self.Proj_grad)
+
+            if gap <= self.gradient_gap_tol:
+                return True
+            else:
+                return False
 
     def fit(self, X_train, y_train, X_valid, y_valid):
         """
@@ -42,32 +105,40 @@ class bilevel_SGD():
         self.C = self.C_min
         feature_size = X_train.shape[1]
 
-        X_t_size = X_train.shape[0]
-        X_v_size = X_valid.shape[0]
-        self.beta = np.random.uniform(-1, 1, feature_size)
+        self.X_t_size = X_train.shape[0]
+        self.X_v_size = X_valid.shape[0]
+
+        # initialize for dual CD step
+        self.Q_ii = (self.X_train*self.X_train).sum(axis=1)
+        self.alpha = np.zeros(self.X_t_size) # initialize alpha
+        self.Gradient = np.ones(self.X_t_size)
+        self.Proj_grad = np.random.uniform(-1,1,self.X_t_size)
+
+        self.beta = np.dot(np.multiply(self.alpha, self.y_train), self.X_train)
 
         t = 1
         # print self.stop()
-        dp = dynamic_plot(xlim=(0,self.t_max), ylim=(0, 1), xlabel = 'Iteration', ylabel = 'error')
+        # dp = dynamic_plot(xlim=(0,self.t_max), ylim=(0, 1), xlabel = 'Iteration', ylabel = 'Accuracy')
 
         while (self.stop() < self.accuracy_threshold) and (t <= self.t_max):
+
+            # update lower level variables
+            self.dual_CD_step()
 
             error_vector_t = np.multiply(y_train, np.dot(X_train, self.beta))
             error_vector_v = np.multiply(y_valid, np.dot(X_valid, self.beta))
 
-            l = np.random.choice(np.where(error_vector_t<1)[0])
+            # l = np.random.choice(np.where(error_vector_t<1)[0])
+            misclassified_train_ind = np.where(error_vector_t<1)[0]
+
+            temp_grad_C = np.dot(y_train[misclassified_train_ind], X_train[misclassified_train_ind, ])
+
             p = np.random.choice(np.where(error_vector_v<1)[0])
 
-            # update lower level gradient
-            G_grad =  self.beta - self.C*y_train[l]*X_train[l,]
-
             # update upper level gradient
-            C_grad = - np.dot(y_valid[p]*X_valid[p,], y_train[l]*X_train[l,])
+            C_grad = - np.dot(y_valid[p]*X_valid[p,], temp_grad_C)
 
             # print C_grad
-            
-            self.beta = self.beta - self.lr_beta*G_grad
-            # print self.beta
 
             self.lr_C = 1/np.absolute(t*np.sqrt(feature_size)*C_grad)
             self.C = self.C - self.lr_C*C_grad
@@ -79,15 +150,18 @@ class bilevel_SGD():
 
             # print self.stop()
             # print self.C
-            dp.update_line(t, self.stop())
+            # dp.update_line(t, self.stop())
+            self.accuracy_ls.append(self.stop())
 
             t += 1
         # print 'final accuracy: ', self.stop()
-        dp.fig.savefig('svmguide1_error_profile_stepC_1_stepW_0.001_one_validation.png')
+        # dp.fig.savefig('pima_error_profile_stepC_1_stepW_0.001_one_validation_Alg2.png')
 
         print 'final C: ', self.C
         print 'final cross-val accuracy: ', self.stop()
-
+        plt.figure()
+        plt.plot(range(0, self.t_max), self.accuracy_ls)
+        plt.show()
 
     def stop(self):
         """
@@ -103,7 +177,7 @@ class bilevel_SGD():
     def _duality_gap(self):
         dual_obj = -0.5* np.dot(self.beta, self.beta) + np.sum(self.alpha)
 
-        prim_obj = 0.5* np.dot(self.beta, self.beta) + self.C * np.sum( np.maximum(1 - np.multiply(np.dot(self.X, self.beta), self.y), 0))
+        prim_obj = 0.5* np.dot(self.beta, self.beta) + self.C * np.sum( np.maximum(1 - np.multiply(np.dot(self.X_train, self.beta), self.y_train), 0))
 
             # print (prim_obj - dual_obj)
         self.gap = prim_obj - dual_obj
@@ -116,6 +190,20 @@ class bilevel_SGD():
     def accuracy(self, y_true, y_pred):
 
         return sum(y_true==y_pred)*1.0/len(y_true)
+
+def pima_data():
+    df = pd.read_csv("data/pima-indians-diabetes.txt")
+
+    X = df.values[:,range(0,8)]
+    y = df.values[:,8]
+
+    X = X[:, np.var(X, axis=0)>0]
+
+    X = np.apply_along_axis(lambda x: (x-min(x))/(max(x)-min(x))*2 -1, axis=0 , arr = X)
+
+    y[y==0] = -1
+    y[y==1] = 1
+    return X, y
 
 def svmguide1():
     df = pd.read_csv("data/svmguide1.csv", header=None)
@@ -136,7 +224,7 @@ def svmguide1():
 if __name__ == '__main__':
     # X = pd.read_csv('../OptimizationProject_Wei/adult_x.csv', header=None)
     # y = pd.read_csv('../OptimizationProject_Wei/adult_y.csv', header=None)
-    X, y = svmguide1()
+    X, y = pima_data()
 
     temp_ind = np.random.randint(X.shape[0], size=X.shape[0]/2)
     val_ind = list(set(range(0, X.shape[0])) - set(temp_ind))
@@ -147,5 +235,5 @@ if __name__ == '__main__':
     y_valid = y[val_ind, ]
 
 
-    bi_SGD = bilevel_SGD()
+    bi_SGD = bilevel_SGD_Alg2()
     bi_SGD.fit(X_train, y_train, X_valid, y_valid)
